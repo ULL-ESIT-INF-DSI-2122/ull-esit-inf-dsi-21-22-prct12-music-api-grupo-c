bcaae7b78b460e20fc869165a4533d721a20d573
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-16 17:29:42 +0100

    adding artist interface

--- src/models/artist.interface.ts
+++ src/models/artist.interface.ts
@@ -1,23 +1,6 @@
-export class Artist {
-  id?: string;
-
-  name: string;
-
-  genres: string[];
-
-  songs: string[];
-
-  listeners: number;
-
-  constructor(
-    name: string,
-    genres: string[],
-    songs: string[],
-    listeners: number,
-  ) {
-    this.name = name;
-    this.genres = genres;
-    this.songs = songs;
-    this.listeners = listeners;
-  }
+interface ArtistInterface {
+  name: string,
+  genres: string[],
+  songs: string[],
+  listeners: number
 }

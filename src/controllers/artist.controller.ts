15116ca101437d15169fe125039cd6074010aae2
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-18 19:05:38 +0100

    adding methods

--- src/controllers/artist.controller.ts
+++ src/controllers/artist.controller.ts
@@ -2,6 +2,25 @@ import { Request, Response } from 'express';
 import { Artist } from '../models/artist.model';
 
 export default {
+  getAllArtists: (req: Request, res: Response) => {
+    // eslint-disable-next-line array-callback-return
+    Artist.find((err, artists) => {
+      if (err) {
+        res.json(err);
+      } else {
+        res.json(artists);
+      }
+    });
+  },
+  getArtist: (req: Request, res: Response) => {
+    Artist.findOne({ name: req.params.artist })
+      .then((result) => {
+        res.status(200).json(result);
+      })
+      .catch(() => {
+        res.status(401).send({ success: false, msg: 'Get failed. Artist not found.' });
+      });
+  },
   addArtist: (req: Request, res: Response) => {
     const newArtist = new Artist({
       name: req.body.name,
@@ -18,15 +37,12 @@ export default {
       });
   },
   deleteArtist: (req: Request, res: Response) => {
-    Artist.deleteOne({
-      name: req.params.artist,
-    }, (err, result) => {
-      if (err) throw err;
-      if (!result) {
-        res.status(401).send({ success: false, msg: 'Update failed. Artist not found.' });
-      } else {
+    Artist.deleteOne({ name: req.params.artist })
+      .then((result) => {
         res.status(200).json(result);
-      }
-    });
+      })
+      .catch((err) => {
+        res.status(401).json({ success: false, msg: err.msg });
+      });
   },
 };

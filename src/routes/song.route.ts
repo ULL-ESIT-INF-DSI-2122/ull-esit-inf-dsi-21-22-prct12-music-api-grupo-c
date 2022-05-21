9447f0489de4b8494922d78a839a8238665dc90a
Alejandro Martín de León <alu0101015941@ull.edu.es>
2022-05-21 13:18:57 +0100

    feat: First version Song CRUD routes done :gear:

---
+++ src/routes/song.route.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import songController from '../controllers/song.controller';
+
+const songRouter: Router = Router();
+
+songRouter.get('/', songController.matchNameQuery, songController.getSongByName);
+songRouter.get('/', songController.getAllSongs);
+songRouter.get('/:id', songController.getSongById);
+songRouter.post('/', songController.addSong);
+songRouter.delete('/:id', songController.deleteSong);
+songRouter.put('/', songController.matchNameQuery, songController.updateSongByName);
+songRouter.put('/:id', songController.updateSongById)
+export default songRouter;
